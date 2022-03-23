00535cffb788c9b7e285e8be03f1c0ca9b21e033
yys7517 <yys7517@naver.com>
2022-03-23 23:50:37 +0900

    사용자의 사용 경험을 바탕으로 Splash 에서 Intent를 다르게 준다.

--- app/src/main/java/com/youngsun/mysololife/SplashActivity.kt
+++ app/src/main/java/com/youngsun/mysololife/SplashActivity.kt
@@ -4,19 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.youngsun.mysololife.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        auth = Firebase.auth
 
-        // 2초 후 IntroActivity로 Intent를 통하여 이동.
-        Handler().postDelayed({
-            val intent = Intent( this, IntroActivity :: class.java )
-            startActivity(intent)
-            finish()
-        },1500)
+        // 현재 사용자의 uid가 없다면, 로그인/회원가입을 위해 IntroActivity 로 이동.
+        if( auth.currentUser!!.uid == null ) {
+            Handler().postDelayed({
+                val intent = Intent( this, IntroActivity :: class.java )
+                startActivity(intent)
+                finish()
+            },1500)
+        }
+        // 사용자가 App을 사용한 적이 있다면, MainActivity 로 이동.
+        else {
+            Handler().postDelayed({
+                val intent = Intent( this, MainActivity :: class.java )
+                startActivity(intent)
+                finish()
+            },1500)
+        }
+
+
+
     }
 }
