b84e3fc5940d76aca3194d742dd2a4ca14821d72
yys7517 <yys7517@naver.com>
2022-03-23 22:36:31 +0900

    비회원 로그인, 로그아웃 기능 구현.

--- app/src/main/java/com/youngsun/mysololife/MainActivity.kt
+++ app/src/main/java/com/youngsun/mysololife/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.youngsun.mysololife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.youngsun.mysololife.auth.IntroActivity
+import com.youngsun.mysololife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView( this, R.layout.activity_main )
+
+        auth = Firebase.auth
+
+        // 로그아웃 버튼
+        binding.btnLogout.setOnClickListener {
+
+            auth.signOut()      // 로그아웃
+
+            val intent = Intent( this, IntroActivity::class.java )
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity( intent )
+        }
+
     }
 }
--- app/src/main/java/com/youngsun/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/youngsun/mysololife/auth/IntroActivity.kt
@@ -3,17 +3,25 @@ package com.youngsun.mysololife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.youngsun.mysololife.MainActivity
 import com.youngsun.mysololife.R
 import com.youngsun.mysololife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView( this, R.layout.activity_intro )
+
+        auth = Firebase.auth
 
         binding = ActivityIntroBinding.inflate( layoutInflater )
         setContentView( binding.root )
@@ -32,7 +40,21 @@ class IntroActivity : AppCompatActivity() {
 
         // 비회원 로그인 버튼 클릭
         binding.btnAnonymous.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 익명 로그인 성공
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
 
+                        val intent = Intent( this, MainActivity::class.java )
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+
+                    } else {
+                        // 실패
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
