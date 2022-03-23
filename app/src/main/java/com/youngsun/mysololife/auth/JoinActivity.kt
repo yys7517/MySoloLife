f4804cacf1475d828c7f03ae0a9328d23553c8dd
yys7517 <yys7517@naver.com>
2022-03-23 22:26:06 +0900

    Firebase Auth 사용하여 로그인 / 회원가입 기능 구현 및 화면 디자인 작업.

--- app/src/main/java/com/youngsun/mysololife/auth/JoinActivity.kt
+++ app/src/main/java/com/youngsun/mysololife/auth/JoinActivity.kt
@@ -1,12 +1,82 @@
 package com.youngsun.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.youngsun.mysololife.MainActivity
 import com.youngsun.mysololife.R
+import com.youngsun.mysololife.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityJoinBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+
+        binding = DataBindingUtil.setContentView( this, R.layout.activity_join )
+
+        auth = Firebase.auth
+
+        binding.btnSubmit.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPass.text.toString()
+            val passCheck = binding.edtPassCheck.text.toString()
+
+            var isGoToJoin  = true
+
+            when {
+                email.isEmpty() -> { // 이메일이 비어있다면
+                    Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    isGoToJoin = false
+                }
+
+                password.isEmpty() -> { // 비밀번호가 비어있다면
+                    Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    isGoToJoin = false
+                }
+
+                password.length < 6 -> { // 비밀번호의 길이가 6보다 짧다면
+                    Toast.makeText(this, "비밀번호를 더 길게 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    isGoToJoin = false
+                    binding.edtPass.text.clear()
+                }
+
+                passCheck.isEmpty() -> { // 비밀번호 확인 값이 비어있다면
+                    Toast.makeText(this, "비밀번호를 한 번 더 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    isGoToJoin = false
+                }
+
+                ! password.equals( passCheck ) -> { // 비밀번호가 일치하지 않는다면
+                    isGoToJoin = false
+                    Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                    binding.edtPassCheck.text.clear()
+                }
+
+                // 회원가입 조건을 만족한다면 ?
+                isGoToJoin -> {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "회원가입 성공!", Toast.LENGTH_SHORT).show()
+                            // 회원가입이 성공하면 메인 화면으로 이동.
+                            val intent = Intent( this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+
     }
 }
--- app/src/main/java/com/youngsun/mysololife/auth/LoginActivity.kt
+++ app/src/main/java/com/youngsun/mysololife/auth/LoginActivity.kt
@@ -1,12 +1,59 @@
 package com.youngsun.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.youngsun.mysololife.MainActivity
 import com.youngsun.mysololife.R
+import com.youngsun.mysololife.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView( this, R.layout.activity_login )
+        auth = Firebase.auth
+
+        binding.btnSubmit.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPass.text.toString()
+
+            when {
+                email.isEmpty() -> { // 이메일이 비어있다면
+                    Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+
+                password.isEmpty() -> { // 비밀번호가 비어있다면
+                    Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    // 로그인 시도
+                    auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                // 로그인 성공 -> 메인 화면
+                                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                                val intent = Intent( this, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                startActivity(intent)
+
+                            } else {
+                                // 로그인 실패
+                                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+            }
+
+
+        }
     }
 }
